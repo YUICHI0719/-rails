8c5a77cef9ca3f01a0b0d21d247f597157403a59
YUICHI KANNO <yuichi@YUICHInoMacBook-Air.local>
2019-11-19 09:33:42 +0900

    Finish Chapter 4

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_090654) do
+ActiveRecord::Schema.define(version: 2019_11_19_003050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "{:null=>false, :index=>true}_id"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+    t.index ["{:null=>false, :index=>true}_id"], name: "index_tasks_on_{:null=>false, :index=>true}_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
